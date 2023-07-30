bdac70d4d39701fdbe4c0103889471a437279c2a
eric <eric2c@outlook.com>
2023-07-31 00:07:01 +0100

    refactor functions

--- my_hdqn_learning.py
+++ my_hdqn_learning.py
@@ -24,7 +24,7 @@ def one_hot_state(state):
     return np.expand_dims(state, axis=0)
 
 def one_hot_goal(goal):
-    vector = np.zeros(4)  ### empty board
+    vector = np.zeros(4)
     vector[goal] = 1.0
     return np.expand_dims(vector, axis=0)
 
@@ -33,27 +33,8 @@ def hdqn_learning(
     agent,
     num_episodes,
     exploration_schedule,
-    gamma=1.0,
+    gamma,
     ):
-
-    """The h-DQN learning algorithm.
-    All schedules are w.r.t. total number of steps taken in the environment.
-    Parameters
-    ----------
-    env: gym.Env
-        gym environment to train on.
-    agent:
-        a h-DQN agent consists of a meta-controller and controller.
-    num_episodes:
-        Number (can be divided by 1000) of episodes to run for. Ex: 12000
-    exploration_schedule: Schedule (defined in utils.schedule)
-        schedule for probability of chosing random action.
-    gamma: float
-        Discount Factor
-    """
-    ###############
-    # RUN ENV     #
-    ###############
     # Keep track of useful statistics
     stats = plotting.EpisodeStats(
         episode_lengths=np.zeros(num_episodes),
@@ -77,7 +58,7 @@ def hdqn_learning(
                 meta_timestep += 1
                 # Get annealing exploration rate (epislon) from exploration_schedule
                 meta_epsilon = exploration_schedule.value(total_timestep)
-                goal = agent.select_goal(encoded_current_state, meta_epsilon)[0]
+                goal = agent.select_action(encoded_current_state, meta_epsilon, goal=True)[0]
                 goals_selection[i_thousand_episode][goal] += 1
                 encoded_goal = one_hot_goal(goal)
 
@@ -90,7 +71,7 @@ def hdqn_learning(
                     # Get annealing exploration rate (epislon) from exploration_schedule
                     ctrl_epsilon = exploration_schedule.value(total_timestep)
                     joint_state_goal = np.concatenate([encoded_current_state, encoded_goal], axis=1)
-                    action = agent.select_action(joint_state_goal, ctrl_epsilon)[0]
+                    action = agent.select_action(joint_state_goal, ctrl_epsilon, goal=False)[0]
                     ### Step the env and store the transition
                     next_state, extrinsic_reward, done, _, _ = env.step(action)
                     # Update statistics
@@ -99,16 +80,15 @@ def hdqn_learning(
                     # visits[i_thousand_episode][next_state-1] += 1
 
                     encoded_next_state = one_hot_state(next_state)
-                    intrinsic_reward = agent.get_intrinsic_reward_lines(goal, extrinsic_reward)
+                    intrinsic_reward = agent.get_intrinsic_reward(goal, extrinsic_reward)
                     
-                    goal_reached = agent.check_goal_lines(goal, extrinsic_reward)
-                    # goal_reached = agent.check_goal(goal, current_state)
+                    goal_reached = agent.check_goal(goal, extrinsic_reward)
 
                     joint_next_state_goal = np.concatenate([encoded_next_state, encoded_goal], axis=1)
                     agent.ctrl_replay_memory.push(joint_state_goal, action, joint_next_state_goal, intrinsic_reward, done)
                     # Update Both meta-controller and controller
-                    agent.update_meta_controller(gamma)
-                    agent.update_controller(gamma)
+                    agent.update_controller(gamma, meta=True)
+                    agent.update_controller(gamma, meta=False)
 
                     total_extrinsic_reward += extrinsic_reward
                     current_state = next_state
@@ -116,7 +96,7 @@ def hdqn_learning(
                 # Goal Finished
                 agent.meta_replay_memory.push(encoded_current_state, goal, encoded_next_state, total_extrinsic_reward, done)
             episode = i_thousand_episode*1000 + i_episode
-            if (episode + 1) % 1000 == 0:
+            if (episode + 1) % 100 == 0:
                 print(f'steps: {total_timestep:6} episode: {episode + 1:6} reward: {stats.episode_rewards[episode]:3} average: {sum(stats.episode_rewards[:episode + 1])/episode + 1:3.2f}')
                 print(f'goals_selection: {goals_selection[i_thousand_episode]}')
     return agent, stats, {}
